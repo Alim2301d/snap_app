d9fa8944a6a0363ef233185d78d7df8bf1d6d9ac
hamzamalik09 <133909516+Alim2301d@users.noreply.github.com>
2024-11-15 12:11:24 +0500

    1st session

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,42 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:snap_app/firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'pages/SplashPage.dart';
+
+void main ()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const Snap_app());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+class Snap_app extends StatelessWidget {
+  const Snap_app({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      title: 'Snap App',
+      debugShowCheckedModeBanner: false,
+      home :const SplashPage(),
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.lightBlue[900],
+        appBarTheme:AppBarTheme(
+          color: Colors.lightBlue[600],
+          backgroundColor: Colors.lightBlue[850]
+        ),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(fontFamily: 'Gothic', color: Colors.greenAccent),
+          bodyMedium: TextStyle(fontFamily: 'Gothic', color: Colors.greenAccent),
+          bodySmall: TextStyle(fontFamily: 'Gothic', color: Colors.greenAccent),
+
+          titleLarge: TextStyle(fontFamily: 'Gothic', color: Colors.orange),
+          titleMedium: TextStyle(fontFamily: 'Gothic', color: Colors.orange),
+          titleSmall: TextStyle(fontFamily: 'Gothic', color: Colors.orange),
+        )
+      ),
+    );
   }
 }
